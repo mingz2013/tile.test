a0145bbc08235fba2ef28583a93dd8c5ea10c039
mingz <305603665@qq.com>
2017-04-07 16:19:41 +0800

    init

--- app/static/app/src/apis.js
+++ app/static/app/src/apis.js
@@ -1,3 +1,43 @@
 /**
  * Created by zhaojm on 07/04/2017.
  */
+
+import jQuery from 'jquery'
+
+const $ = jQuery.$;
+
+const get_game_id_list = (callback) => {
+    $.ajax({
+        url: "/game_id_list",
+        dataType: "json",
+        method: "GET"
+    }).success((data, textStatus, jqXHR)=> {
+        callback(data);
+    })
+};
+
+const get_play_mode_list_by_game_id = (game_id, callback) => {
+    $.ajax({
+        url: "/play_mode_list/" + game_id,
+        dataType: "json",
+        method: "GET"
+    }).success((data, textStatus, jqXHR)=> {
+        callback(data);
+    })
+};
+
+const get_templ_by_templ_name = (templ_name, callback) => {
+    $.ajax({
+        url: "/templ/" + templ_name,
+        dataType: "json",
+        method: "GET"
+    }).success((data, textStatus, jqXHR)=> {
+        callback(data);
+    })
+};
+
+export default {
+    get_game_id_list,
+    get_play_mode_list_by_game_id,
+    get_templ_by_templ_name
+}
--- app/static/app/src/interface.js
+++ app/static/app/src/interface.js
@@ -2,3 +2,13 @@
  * Created by zhaojm on 07/04/2017.
  */
 
+
+const init = () => {
+
+
+};
+
+
+Window.onload = () => {
+
+};
