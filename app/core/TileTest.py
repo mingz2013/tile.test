b65e3ba516cedaa22fb49c19f6d2917c275d8975
mingz <305603665@qq.com>
2017-04-07 12:30:16 +0800

    init

---
+++ app/core/TileTest.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+'''
+Created on 07/04/2017
+
+@author: zhaojingming@tuyoogame.com
+'''
+
+
+class TileTest(object):
+    def __init__(self):
+        pass
---
+++ app/core/database.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+'''
+Created on 07/04/2017
+
+@author: zhaojingming@tuyoogame.com
+'''
+
+import redis
