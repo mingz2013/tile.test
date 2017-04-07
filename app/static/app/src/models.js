45d0c9b4b9a5fb4b5447d6c536d99aad62333aa4
mingz <305603665@qq.com>
2017-04-07 21:41:44 +0800

    init

--- app/static/app/src/models.js
+++ app/static/app/src/models.js
@@ -3,12 +3,44 @@
  */
 
 
+export default (()=> {
 
-const Models = {
-    game_id_list: [],
-    play_mode_list: [],
-    tiles: []
-};
+    let game_id_list = [];
+    let play_mode_list = [];
+    let templ = {};
+
+    let get_game_id_list = () => {
+        return game_id_list;
+    };
+
+    let set_game_id_list = (game_ids) => {
+        game_id_list = game_ids;
+    };
+
+    let get_play_mode_list = ()=> {
+        return play_mode_list;
+    };
+    let set_play_mode_list = (play_modes) => {
+        play_mode_list = play_modes;
+    };
+
+    let get_templ = () => {
+        return templ;
+    };
+
+    let set_templ = (t) => {
+        templ = t;
+    };
 
 
-export default Models;
+    return {
+        get_game_id_list,
+        set_game_id_list,
+        get_play_mode_list,
+        set_play_mode_list,
+        get_templ,
+        set_templ
+    }
+
+
+})();
